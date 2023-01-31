51b705d836d8c823ea07d7e5868fabddc6c1f00d
bakkar, ayoub <ayoub.bakkar@adesso.de>
2023-01-31 12:08:55 +0100

    create navbar and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main/main.component';
+import { ProfilComponent } from './profil/profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
