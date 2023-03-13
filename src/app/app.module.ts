c975ec5bebf5e539418cebcd0075d00266a17bda
bakkar, ayoub <ayoub.bakkar@adesso.de>
2023-03-13 08:54:46 +0100

    add all nececarry components to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main/main.component';
 import { ProfilComponent } from './profil/profil/profil.component';
+import { AvatarComponent } from './shared/avatar/avatar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoriesHeaderComponent } from './main/stories-header/stories-header.component';
+import { PostComponent } from './shared/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MainComponent,
-    ProfilComponent
+    ProfilComponent,
+    AvatarComponent,
+    
+    StoriesHeaderComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
