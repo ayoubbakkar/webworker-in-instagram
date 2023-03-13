9107b950966f1750ed3ec47b83aa443bcd93eeb6
bakkar, ayoub <ayoub.bakkar@adesso.de>
2023-03-13 10:30:02 +0100

    compress all images and save them as images.tar.gz

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+/*const gulp = require('gulp');
 let imagemin = null;
 let rename = null;
 let size = null;
@@ -16,7 +16,7 @@ async function compressImages() {
   // Log the size of the compressed files
   const compressedSize = size.default({ showFiles: true, pretty: true });
 
-  return gulp.src('src/assets/images/**/*.{jpg,jpeg,png,gif,svg}')
+  return gulp.src('src/assets/images/**.{jpg,jpeg,png,gif,svg}')
     // Log the size of the uncompressed files before compression
     .pipe(uncompressedSize)
     .pipe(imagemin.default())
@@ -26,6 +26,30 @@ async function compressImages() {
       file.dirname = file.dirname.replace('/images', '/compressed-images');
     }))
     .pipe(gulp.dest('src/assets/compressed-images'));
+}
+
+gulp.task('compress-images', compressImages);*/
+
+const gulp = require('gulp');
+const tar = require('gulp-tar');
+const gzip = require('gulp-gzip');
+const size = require('gulp-size');
+
+function compressImages() {
+  // Log the size of the uncompressed files
+  const uncompressedSize = size({ showFiles: true, pretty: true });
+
+  // Log the size of the compressed files
+  const compressedSize = size({ showFiles: true, pretty: true });
+
+  return gulp.src('src/assets/images/**/*.{jpg,jpeg,png,gif,svg}')
+    // Log the size of the uncompressed files before compression
+    .pipe(uncompressedSize)
+    .pipe(tar('images.tar'))
+    .pipe(gzip())
+    // Log the size of the compressed files after compression
+    .pipe(compressedSize)
+    .pipe(gulp.dest('./src/assets/compressed-images'));
 }
 
 gulp.task('compress-images', compressImages);
